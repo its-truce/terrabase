e72560876b2278487c2d1823e8c383e883e32362
itstruce <107202816+its-truce@users.noreply.github.com>
2023-03-11 14:34:05 +0530

    Create config.py

---
+++ main/config.py
@@ -0,0 +1,4 @@
+token = "YOUR TOKEN HERE" # your bot's token
+prefix = "~" # recommended not to change
+owner_id = "YOUR ID HERE" # your id
+initial_extensions = ("extensions.wiki",) # your extensions
